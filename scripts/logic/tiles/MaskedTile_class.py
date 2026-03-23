71b76e9cadd6606131adc2096f5338d5d3ff9783
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-23 13:50:01 +0100

    feat : create Tile class and immediate children

---
+++ scripts/logic/tiles/MaskedTile_class.py
@@ -0,0 +1,6 @@
+# A tile that hasn't been revealed yet
+
+from Tile_class import Tile 
+
+class MaskedTile(Tile) : 
+    pass
---
+++ scripts/logic/tiles/Tile_class.py
@@ -0,0 +1,4 @@
+# A Tile that composes the game grid
+
+class Tile : 
+    pass
