2a8455fc1dfa186f64931f3742915f7ae8e5cafc
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-03-27 15:31:46 +0100

    refactor : clean up main.py into functions to make loop simpler, add comments and docstrings

--- main.py
+++ main.py
@@ -8,113 +8,149 @@ from scripts.ui.purchase    import Purchase
 from scripts.ui.game_screen import GameScreen
 from scripts.ui.ui_settings import *
 
+def init_game():
+    """ Initialize Pygame, variables, game states"""
 
-def main():
-
+    # Pygame init
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_W, WINDOW_H))
     pygame.display.set_caption(WINDOW_TITLE)
-    clock  = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
-    home        = HomeMenu(screen)
-    options     = Options(screen)
-    shop        = Shop(screen)
-    purchase    = Purchase(screen)
-    game_screen = None
+    # UI Screens
+    screens = {
+        SCREEN_HOME:     HomeMenu(screen),
+        SCREEN_OPTIONS:  Options(screen),
+        SCREEN_SHOP:     Shop(screen),
+        SCREEN_PURCHASE: Purchase(screen),
+        SCREEN_GAME:     None
+    }
 
-    current       = SCREEN_HOME         
-    previous      = SCREEN_HOME   # Memorize previous screen
+    # Global state
+    state = {
+        "current": SCREEN_HOME,
+        "previous": SCREEN_HOME,
+        "unlocked_skins": ["default"],
+        "ads_removed": False,
+        "game_screen": None
+    }
 
-    # Skins
-    unlocked_skins = ["default"]
-    # Tracks whether the ads have been removed 
-    ads_removed    = False        
+    return screen, clock, screens, state
+
+def handle_events(current, screens, state):
+    """ Return an action string or tuple, or None. """
+    for event in pygame.event.get():
+
+        # Quit window with red cross
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # Listen to events of the current screen (see its own script for .handle_event() method)
+        screen_obj = screens[current] if current != SCREEN_GAME else state["game_screen"]
+        if screen_obj:
+            action = screen_obj.handle_event(event)
+            if action:
+                return action
+
+    return None
+
+def apply_actions(action, screens, state):
+
+    if action is None:
+        return
+
+    current = state["current"]
+
+    # Simple transitions
+    if action == "options":
+        state["current"] = SCREEN_OPTIONS
+
+    elif action == "shop":
+        state["previous"] = current
+        state["current"] = SCREEN_SHOP
+
+    elif action == "home":
+        state["current"] = SCREEN_HOME
+
+    elif action == "back":
+        if state["previous"] == SCREEN_GAME:
+            state["game_screen"].resume()
+            state["current"] = SCREEN_GAME
+        else:
+            state["current"] = SCREEN_HOME
+
+    # Ads purchased
+    elif action == "ads purchased":
+        state["ads_removed"] = True
+        screens[SCREEN_PURCHASE].play_coo()
+        screens[SCREEN_HOME].ads_removed = True
+        if state["game_screen"]:
+            state["game_screen"].ads_removed = True
+        state["current"] = SCREEN_PURCHASE
+
+    # Skin purchased
+    elif action[0] == "flag_purchased":
+        skin = action[1]
+        if skin not in state["unlocked_skins"]:
+            state["unlocked_skins"].append(skin)
+        screens[SCREEN_SHOP].mark_purchased(skin)
+        screens[SCREEN_PURCHASE].play_coo()
+        state["current"] = SCREEN_PURCHASE
+
+    # New game
+    elif action[0] == "new_game":
+        new_game, grid_size, num_bombs = action
+        print(action)
+        difficulty = screens[SCREEN_HOME].btn_difficulty.current
+
+        state["game_screen"] = GameScreen(
+            screens[SCREEN_HOME].screen,
+            grid_size, num_bombs, difficulty,
+            state["unlocked_skins"],
+            state["ads_removed"]
+        )
+
+        state["current"] = SCREEN_GAME
+
+def draw(current, screens, state, dt, dt_ms):
+    """ Draw all elements of the current screen state on the window """
+
+    if current == SCREEN_HOME:
+        screens[SCREEN_HOME].draw()
+        screens[SCREEN_HOME].left_coin.update(dt)
+        screens[SCREEN_HOME].right_coin.update(dt)
+
+    elif current == SCREEN_OPTIONS:
+        screens[SCREEN_OPTIONS].draw()
+
+    elif current == SCREEN_SHOP:
+        screens[SCREEN_SHOP].draw()
+
+    elif current == SCREEN_PURCHASE:
+        screens[SCREEN_PURCHASE].draw()
+
+    elif current == SCREEN_GAME :
+        state["game_screen"].update(dt_ms)
+        state["game_screen"].draw()
+
+    pygame.display.flip()
+
+def main():
+
+    screen, clock, screens, state = init_game()
 
     while True:
+        dt_ms = clock.tick(FPS)
+        dt = dt_ms / 1000
 
-        dt_ms = clock.tick(FPS)          # microseconds
-        dt    = dt_ms / 1000             # seconds
+        action = handle_events(state["current"], screens, state)  
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        apply_actions(action, screens, state)
 
-            action = None
-
-            # What events are listened to depending on screen
-            if current == SCREEN_HOME:
-                action = home.handle_event(event)
-            elif current == SCREEN_OPTIONS:
-                action = options.handle_event(event)
-            elif current == SCREEN_SHOP:
-                action = shop.handle_event(event)
-            elif current == SCREEN_PURCHASE:
-                action = purchase.handle_event(event)
-            elif current == SCREEN_GAME and game_screen:
-                action = game_screen.handle_event(event)
-
-            # Transitions
-
-            if action == "options":
-                current = SCREEN_OPTIONS
-
-            elif action == "shop":
-                previous = current          # Remembers what screen was before (game or menu)
-                current  = SCREEN_SHOP
-
-            elif action == "home":
-                current = SCREEN_HOME
-
-            elif action == "back":
-                # Back from shop
-                if previous == SCREEN_GAME and game_screen:
-                    game_screen.resume()    # Resume timer
-                    current = SCREEN_GAME
-                else:
-                    current = SCREEN_HOME
-
-            elif action == "ads purchased":
-                ads_removed = True                          # ← NEW : update global flag
-                purchase.play_coo()
-                home.ads_removed = True
-                if game_screen:                             # ← NEW : hide ads in current game
-                    game_screen.ads_removed = True
-                current = SCREEN_PURCHASE
-
-            elif isinstance(action, tuple) and action[0] == "flag_purchased":
-                skin = action[1]
-                if skin not in unlocked_skins:
-                    unlocked_skins.append(skin)
-                shop.mark_purchased(skin)
-                purchase.play_coo()
-                current = SCREEN_PURCHASE
- 
-            elif isinstance(action, tuple) and action[0] == "new_game":
-                _, grid_size, num_bombs = action
-                difficulty  = home.btn_difficulty.current  # "easy" | "medium" | "hard"
-                game_screen = GameScreen(screen, grid_size, num_bombs, difficulty,
-                                         unlocked_skins, ads_removed)  # ← NEW : pass ads_removed
-                current     = SCREEN_GAME
-
-        # rendering
-
-        if current == SCREEN_HOME:
-            home.draw()
-            home.left_coin.update(dt)
-            home.right_coin.update(dt)
-        elif current == SCREEN_OPTIONS:
-            options.draw()
-        elif current == SCREEN_SHOP:
-            shop.draw()
-        elif current == SCREEN_PURCHASE:
-            purchase.draw()
-        elif current == SCREEN_GAME and game_screen:
-            game_screen.update(dt_ms)   # Animate ad
-            game_screen.draw()
-
-        pygame.display.flip()
+        draw(state["current"], screens, state, dt, dt_ms)
 
 
 if __name__ == "__main__":
+
     main()
